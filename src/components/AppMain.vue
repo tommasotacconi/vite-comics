3838ee886c9f175b4803b49b20c7d75bf3a32fc1
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-09-16 21:07:28 +0200

    added main component

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -9,11 +9,24 @@ export default {
 </script>
 
 <template>
-  
+  <main>
+    <p>--> Main content goes here<--</p>
+  </main>
 </template>
 
 <style lang="scss" scoped>
-  @use '../styles/general.scss' as *
+  @use '../styles/general.scss' as *;
 
+  main {
+    min-height: 200px;
+    font-size: 1.2rem;
+    color: #fff;
+    background-color: #333;
+  }
 
+  p {
+    height: 100%;
+    padding: 100px 0;
+    vertical-align: middle;
+  }
 </style>
