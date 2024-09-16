5fbe321d12bc02c4013db646ab7ef97d5f57ea14
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-09-16 17:47:33 +0200

    added firt structure

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
-<script setup>
+<script>
+import AppHeader from './components/AppFooter.vue'
+import AppMain from './components/AppMain.vue'
+import AppFooter from './components/AppFooter.vue'
 
+export default {
+  data () {
+    return {
+    }
+  },
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter
+  }
+}
 </script>
 
 <template>
-
+  <AppHeader />
+  <AppMain />
+  <AppFooter />
 </template>
 
 <style scoped>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<template>
+  <header>
+    <div class="logo-box">
+      Logo
+    </div>
+    <nav>
+      <ul>
+        <li>Elemento lista</li>
+        <li>Elemento lista</li>
+        <li>Elemento lista</li>
+        <li>Elemento lista</li>
+        <li>Elemento lista</li>
+        <li>Elemento lista</li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+</style>
