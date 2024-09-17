fc6e83832afdcac28853ec3256b08ba2b19645cc
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-09-17 17:58:34 +0200

    added props

---
+++ src/components/AppMainCard.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  props: [
+    'Thumb',
+    'Series'
+  ]
+}
+</script>
+
+<template>
+  <div class="card">
+    <div class="card-img">
+      <img :src="Thumb" alt="img of {{ Series }}">
+    </div>
+    <h2>
+      {{ Series }}
+    </h2>
+  </div>  
+</template>
+
+<style scoped>
+</style>
